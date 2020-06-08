e355aee20c087f06cb7b40a6b08c398e18728828
mehdi <mahdinickhoo@gmail.com@users.noreply.github.com>
2020-06-08 18:12:46 +0430

    dsds

--- app.js
+++
@@ -1,9 +0,0 @@
-// const express = require('express');
-
-// var app = express(); 
-// app.use(bodyParser.json());
-
-// app.get('/' , function(req,res){
-//     res.writeHead(200 , {'Content-Type': 'text/html'});
-//     res.write("salam");
-// });
