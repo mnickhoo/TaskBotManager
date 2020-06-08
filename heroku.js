27f393027b4b47a0d71f662a3ff6878029cad74d
mehdi <mahdinickhoo@gmail.com@users.noreply.github.com>
2020-06-08 16:56:35 +0430

    haminjoori

--- heroku.js
+++ heroku.js
@@ -34,39 +34,39 @@ const bot = new TelegramBot(TOKEN, options);
 // Note: we do not need to pass in the cert, as it already provided
 bot.setWebHook(`${urlConfig}/bot${TOKEN}`);
 
-app.get('/insert', function(req , res){
-  var q = url.parse(req.url, true); //get url 
-  var filename = "./views/insert.html" //find fileName
-  fs.readFile(filename,function(err, data) {
-    if(err) {
-      res.writeHead(404,{'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
-    } //Error 404 not Found
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    if(q.query.title != null){
-      //initiate to project object
+// app.get('/insert', function(req , res){
+//   var q = url.parse(req.url, true); //get url 
+//   var filename = "./views/insert.html" //find fileName
+//   fs.readFile(filename,function(err, data) {
+//     if(err) {
+//       res.writeHead(404,{'Content-Type': 'text/html'});
+//       return res.end("404 Not Found");
+//     } //Error 404 not Found
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     if(q.query.title != null){
+//       //initiate to project object
 
-      let newProject = new projectModel({
-          title : q.query.title , 
-          description : q.query.description, 
-          linkInfo : q.query.linkInfo , 
-          expireDate : q.query.expireDate, 
-          point : q.query.point
-      }); 
-      //save to db 
-      newProject.save().then((project)=>{
-          //send project to channel
-          taskManager.SendToChannel(chanel_id , project); //send Task to Channel  
+//       let newProject = new projectModel({
+//           title : q.query.title , 
+//           description : q.query.description, 
+//           linkInfo : q.query.linkInfo , 
+//           expireDate : q.query.expireDate, 
+//           point : q.query.point
+//       }); 
+//       //save to db 
+//       newProject.save().then((project)=>{
+//           //send project to channel
+//           taskManager.SendToChannel(chanel_id , project); //send Task to Channel  
 
-          console.log("project has been saved." , project);     
-      },(err) =>{
-          console.log("project unable to save on db", err);
-      });
-    }
-    res.write(data); //return view
-    return res.end();
-  });
-});
+//           console.log("project has been saved." , project);     
+//       },(err) =>{
+//           console.log("project unable to save on db", err);
+//       });
+//     }
+//     res.write(data); //return view
+//     return res.end();
+//   });
+// });
 
 app.post(`/bot${TOKEN}`, (req, res) => {
   bot.processUpdate(req.body);
